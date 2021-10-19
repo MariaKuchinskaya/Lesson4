dd900802d7e2c4fa747ca32acebe2b97acbfc64b
Maria Kuchinskaya <mariakuchinskaya16@gmail.com>
2021-10-19 23:11:58 +0300

    Lesson_4

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 0 && number <= 30)
+            {
+                Console.WriteLine($"{number} is from 0 to 30");
+            }
+            else
+            {
+                if (number >= 31 && number <= 60)
+                {
+                    Console.WriteLine($"{number} is from 31 to 60");
+                }
+                else
+                {
+                    if (number >= 61 && number <= 100)
+                    {
+                        Console.WriteLine($"{number} is from 61 to 100");
+                    }
+                    else
+                    {
+                        Console.WriteLine("I don't know such number");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[300];
+            for (int i = 0; i < 300; i++)
+            {
+                array[i] = 200 + i;
+            }
+
+            var x = 0;
+            while (x < 299)
+            {
+                if (array[x] % 17 == 0)
+                {
+                    Console.WriteLine(array[x]);
+                }
+                x++;
+            }
+
+            // версия без массива
+            //var counter = 0;
+            //while (counter <300)
+            //{
+            //    if (200 + counter % 17 == 0)
+            //    {
+            //        Console.WriteLine(200 + counter);
+            //    }
+            //    counter++;
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        }
+        
+        
+
+    }
+}
